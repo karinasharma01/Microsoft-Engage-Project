291f37f99afde0131e619881f4c44dbbb316e9d2
Karina <karinasharma1200@gmail.com>
2021-07-04 10:47:23 +0530

    Initial commit

---
+++ public/script.js
@@ -0,0 +1,316 @@
+const socket = io('/');  //socket connection
+const videoGrid = document.getElementById('video-grid');
+const userDropDown = document.getElementById('myDropdown');
+const myVideo = document.createElement('video');
+myVideo.muted = true;
+let peers = {}, currentPeer = [];
+let userlist = [];
+let cUser;
+
+const myName = prompt('Enter your name');
+console.log(myName);
+
+var peer = new Peer(undefined, {
+  host: '/',
+  port: '3001'
+});
+
+let myVideoStream;
+navigator.mediaDevices.getUserMedia({
+  audio: true,
+  video: true
+}).then((stream) => {
+  myVideoStream = stream;
+  addVideoStream(myVideo, stream);
+
+
+  peer.on("call", call => {
+    //var callAccepted = confirm("Incoming VideoCall");
+    console.log("Answered");
+      call.answer(stream);
+      const video = document.createElement('video');
+      call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+      });
+      currentPeer.push(call.peerConnection);
+      call.on("close", function () {
+        video.remove();
+        peers[call.peer] = call;
+        alert("The videocall has finished");
+      });
+    });
+    /*
+    if (callAccepted) {
+      console.log("Answered");
+      call.answer(stream);
+      const video = document.createElement('video');
+      call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+      });
+      //currentPeer = call.peerConnection;
+      currentPeer.push(call.peerConnection);
+      // Handle when the call finishes
+      call.on("close", function () {
+        video.remove();
+        peers[call.peer] = call;
+        alert("The videocall has finished");
+      });
+      // use call.close() to finish a call
+    } else {
+      console.log("Call denied !");
+    }
+  });
+*/
+  socket.on('user-connected', (userId) => {
+    console.log('user ID fetch connection: ' + userId);
+    connectToNewUser(userId, stream);
+  })
+
+});
+
+
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close();
+  console.log('user ID fetch Disconnect: ' + userId);
+});
+
+
+const connectToNewUser = (userId, stream) => {
+  console.log('User-connected :-' + userId);
+  const call = peer.call(userId, stream);
+  const video = document.createElement('video');
+  call.on('stream', userVideoStream => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on('close', () => {
+    video.remove()
+  });
+  //currentPeer = call.peerConnection;
+  peers[userId] = call;
+  currentPeer.push(call.peerConnection);
+  console.log(currentPeer);
+};
+
+peer.on('open', async id => {
+  cUser = id;
+  await socket.emit('join-room', ROOM_ID, id, myName);
+
+})
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.controls = true;
+  video.addEventListener('loadedmetadata', () => {
+    video.play();
+  })
+  videoGrid.append(video);
+}
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setMuteButton();
+  } else {
+    myVideoStream.getAudioTracks()[0].enabled = true;
+    setUnmuteButton();
+  }
+}
+
+const setUnmuteButton = () => {
+  const html = `<i class="fas fa-microphone-alt"></i>
+                <span>Mute</span>`;
+  document.querySelector('.Mute__button').innerHTML = html;
+  console.log("You are Unmuted");
+}
+
+const setMuteButton = () => {
+  const html = `<i class="fas fa-microphone-alt-slash" style="color:red;"></i>
+                <span>Unmute</span>`;
+  document.querySelector('.Mute__button').innerHTML = html;
+  console.log("Muted");
+}
+
+
+//Video ON or OFF
+const videoOnOff = () => {
+  const enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    unsetVideoButton();
+  } else {
+    setVideoButton();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setVideoButton = () => {
+  const html = `<i class="fas fa-video"></i>
+                <span>Stop Video</span>`;
+  document.querySelector('.Video__button').innerHTML = html;
+  console.log("Cammera Mode ON");
+}
+
+const unsetVideoButton = () => {
+  const html = `<i class="fas fa-video-slash" style="color:red;"></i>
+                <span>Start Video</span>`;
+  document.querySelector('.Video__button').innerHTML = html;
+  console.log("Cammera Mode OFF");
+}
+
+//code for disconnect from client
+const disconnectNow = () => {
+  window.location = "http://localhost:3000/user";
+}
+
+//code to share url of roomId
+/*const share = () => {
+  var share = document.createElement('input'),
+    text = window.location.href;
+
+  console.log(text);
+  document.body.appendChild(share);
+  share.value = text;
+  share.select();
+  document.execCommand('copy');
+  document.body.removeChild(share);
+  alert('Copied');
+}*/
+
+inviteButton.addEventListener("click", (e) => {
+  prompt(
+    "Copy this link and send it to people you want to meet with",
+    window.location.href
+  );
+});
+
+//msg sen from user
+let text = $('input');
+
+$('html').keydown((e) => {
+  if (e.which == 13 && text.val().length !== 0) {
+    console.log(text.val());
+    socket.emit('message', text.val(), myName);
+    text.val('')
+  }
+});
+
+//Print msg in room
+socket.on('createMessage', (msg, user) => {
+  $('ul').append(`<li class= "message"><small>${user === myName ? "Me" : user}</small><br>${msg}</li>`);
+  scrollToBottom();
+});
+
+const scrollToBottom = () => {
+  var d = $('.main__chat_window');
+  d.scrollTop(d.prop("scrollHeight"));
+}
+
+//screenShare
+const screenshare = () => {
+  navigator.mediaDevices.getDisplayMedia({
+    video: {
+      cursor: 'always'
+    },
+    audio: {
+      echoCancellation: true,
+      noiseSuppression: true
+    }
+
+  }).then(stream => {
+    let videoTrack = stream.getVideoTracks()[0];
+    videoTrack.onended = function () {
+      stopScreenShare();
+    }
+    for (let x = 0; x < currentPeer.length; x++) {
+
+      let sender = currentPeer[x].getSenders().find(function (s) {
+        return s.track.kind == videoTrack.kind;
+      })
+
+      sender.replaceTrack(videoTrack);
+    }
+
+  })
+
+}
+
+function stopScreenShare() {
+  let videoTrack = myVideoStream.getVideoTracks()[0];
+  for (let x = 0; x < currentPeer.length; x++) {
+    let sender = currentPeer[x].getSenders().find(function (s) {
+      return s.track.kind == videoTrack.kind;
+    })
+    sender.replaceTrack(videoTrack);
+  }
+}
+
+//raised hand
+const raisedHand = () => {
+  const displayText = `${myName} has raised hand.`;
+  socket.emit('message', displayText, myName);
+  unChangeHandLogo();
+}
+
+const unChangeHandLogo = () => {
+  const html = `<i class="far fa-hand-paper" style="color:red;"></i>
+                <span>Raised</span>`;
+  document.querySelector('.raisedHand').innerHTML = html;
+  console.log("change")
+  changeHandLogo();
+}
+
+const changeHandLogo = () => {
+  setInterval(function () {
+    const html = `<i class="far fa-hand-paper" style="color:"white"></i>
+                <span>Hand</span>`;
+    document.querySelector('.raisedHand').innerHTML = html;
+  }, 3000);
+}
+
+//kick option
+
+socket.on('remove-User', (userId) => {
+  if (cUser == userId) {
+    disconnectNow();
+  }
+});
+
+const getUsers = () => {
+  socket.emit('seruI',);
+
+}
+
+const listOfUser = () => {
+  //userDropDown.innerHTML = '';
+  while (userDropDown.firstChild) {
+    userDropDown.removeChild(userDropDown.lastChild);
+  }
+  for (var i = 0; i < userlist.length; i++) {
+    var x = document.createElement("a");
+    var t = document.createTextNode(`VideoSector ${i + 1}`);
+    x.appendChild(t);
+    userDropDown.append(x);
+  }
+  const anchors = document.querySelectorAll('a');
+  for (let i = 0; i < anchors.length; i++) {
+    anchors[i].addEventListener('click', () => {
+      console.log(`Link is clicked ${i}`);
+      anchoreUser(userlist[i]);
+    });
+  }
+}
+
+const anchoreUser = (userR) => {
+  socket.emit('removeUser', cUser, userR);
+}
+
+
+socket.on('all_users_inRoom', (userI) => {
+  console.log(userI);
+  userlist.splice(0, userlist.length);
+  userlist.push.apply(userlist, userI);
+  console.log(userlist);
+  listOfUser();
+  document.getElementById("myDropdown").classList.toggle("show");
+});
---
+++ server.js
@@ -0,0 +1,209 @@
+require('dotenv').config()
+
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate')
+
+const app = express();
+const ejs = require('ejs')
+const server = require('http').Server(app);
+const { v4: uuidV4 } = require('uuid');
+const io = require("socket.io")(server);
+const userS = [], userI = [];
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(session ({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect('mongodb+srv://admin-karina:Karina002@cluster0.egpxh.mongodb.net/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useCreateIndex',true);
+
+const userSchema = new mongoose.Schema ({
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+    googleId: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+const User = new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "http://localhost:3000/auth/google/user",
+  userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+},
+function(accessToken, refreshToken, profile, cb) {
+  console.log(profile);
+  User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    return cb(err, user);
+  });
+}
+));
+
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get("/auth/google/user",
+  passport.authenticate('google', { failureRedirect: "/signin" }),
+  function(req, res) {
+    // Successful authentication, redirect to user.
+    res.redirect("/user");
+  });
+
+app.get("/signup", function (req, res) {
+    res.render("signup")
+})
+
+app.get("/signin", function (req, res) {
+    res.render("signin")
+})
+
+app.get("/user", function (req, res) {
+    if(req.isAuthenticated()){
+        res.render("user");
+    }
+    else {
+        res.redirect("/signin");
+    }
+});
+
+app.get("/logout", function(req,res) {
+  req.logout();
+  res.redirect("/");
+})
+
+app.post("/signup", function (req, res) {
+    User.register({firstname:req.body.firstname,lastname:req.body.lastname,username: req.body.username}, req.body.password, function(err, user){
+        if (err) {
+          console.log(err);
+          res.redirect("/signup");
+        } else {
+          passport.authenticate("local")(req, res, function(){
+            res.redirect("/user");
+          });
+        }
+      });
+});
+
+app.post("/signin", function (req, res) {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+      });
+    
+      req.login(user, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          passport.authenticate("local")(req, res, function(){
+            res.redirect("/user");
+          });
+        }
+      });
+});
+
+
+app.post("/user", function (req, res) {
+    const meet = req.body.meetURL;
+    console.log(meet);
+    res.redirect(meet);
+})
+
+app.get('/room', (req, res) => {
+  
+    if(req.isAuthenticated()){
+      res.redirect(`/${uuidV4()}`);
+    }
+    else {
+      res.redirect("/signin");
+    }
+
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+
+        userS.push(socket.id);
+        userI.push(userId);
+        console.log("room Id:- " + roomId, "userId:- " + userId);
+        socket.join(roomId);
+        socket.broadcast.to(roomId).emit('user-connected', userId);
+        socket.on('removeUser', (sUser, rUser) => {
+            var i = userS.indexOf(rUser);
+            if (sUser == userI[0]) {
+                console.log("SuperUser Removed" + rUser);
+                socket.broadcast.to(roomId).emit('remove-User', rUser);
+            }
+        });
+        
+        socket.on('message', (message, yourName) => {
+            io.to(roomId).emit('createMessage', message, yourName);
+
+        })
+
+        socket.on('disconnect', () => {
+            //userS.filter(item => item !== userId);
+            var i = userS.indexOf(socket.id);
+            userS.splice(i, 1);
+            socket.broadcast.to(roomId).emit('user-disconnected', userI[i]);
+            //update array
+
+            userI.splice(i, 1);
+        });
+        socket.on('seruI', () => {
+            socket.emit('all_users_inRoom', userI);
+            //console.log(userS);
+            console.log(userI);
+        });
+        
+    })
+
+})
+
+
+server.listen(process.env.PORT||3000, function () {
+    console.log("Server is running on port 3000");
+});
+
